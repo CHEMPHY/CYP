30da6c0385a628e8c2223659e413ce5ac53b4ba1
Mengyuan Zhu <zhumengyuan@outlook.com>
2015-08-16 00:01:26 -0400

    cyp 2d6 substrate

---
+++ cyp_2d6_substrate/training_set_property.py
@@ -0,0 +1,59 @@
+import pybel
+line=0
+inputfile=pybel.readfile("smi","cyp2d6_substrate_fixpka.smi")
+outfile=open("cyp2d6_substrate_fixpka_property.txt", 'w')
+
+num_molecule=0
+
+for mol in inputfile:
+
+	outfile.write(mol.title)
+	
+	outfile.write(" ")
+	descvalues=mol.calcdesc()
+	outfile.write(str(descvalues.get('TPSA')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('HBD')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('logP')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('MW')))
+	outfile.write(" ")				
+	outfile.write(str(descvalues.get('tbonds')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('nF')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('bonds')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('atoms')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('HBA1')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('HBA2')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('sbonds')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('dbonds')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('MR')))
+	outfile.write(" ")
+	outfile.write(str(descvalues.get('abonds')))
+	outfile.write(" ")
+	smarts = pybel.Smarts("[+]")
+	num=smarts.findall(mol)				
+	outfile.write(str(len(num)))			
+	outfile.write(" ")
+	smarts = pybel.Smarts("[-]")
+	num=smarts.findall(mol)				
+	outfile.write(str(len(num)))
+	outfile.write(" ")			
+	
+
+	outfile.write("\n")
+	line=0
+
+
+
+
+
+
